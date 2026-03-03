ec1e5dd4ac6f95e5618f1723721b4ace4e4fa594
DhritiAgarwal130307 <dhritiagarwalofficial@gmail.com>
2026-03-03 15:27:11 +0530

    Update files

---
+++ setup.sh
@@ -0,0 +1,5 @@
+git init 
+git remote add origin "https://github.com/DhritiAgarwal130307/html-css-Projects-upto-53-videos.git"
+git config user.email "dhritiagarwalofficial@gmail.com"
+git config user.name "DhritiAgarwal130307"
+echo "Setup Complete!"
